138e8e60b6d7e5f6b5e0f15794b87929ab85c078
ChaiyoKung <kananek45@gmail.com>
2025-04-30 22:34:06 +0700

    feat: integrate Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,21 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwindcss from "@tailwindcss/vite";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.chaiyokung.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
