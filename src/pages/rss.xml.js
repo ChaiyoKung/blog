948c7fcd715b387b65cef32626637ef3d3713caa
ChaiyoKung <45798076+ChaiyoKung@users.noreply.github.com>
2025-09-27 01:03:37 +0700

    Feat: Add RSS icon to header and add content to RSS XML (#8)
    * feat: add Markdown parsing and sanitization to RSS feed generation
    
    * fix: ensure trailingSlash is set to false in RSS feed generation
    
    * feat: add XSL stylesheet reference to RSS feed
    
    * feat: add RSS link and icon to header

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,10 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const blogs = await getCollection("blog");
@@ -8,9 +12,15 @@ export async function GET(context) {
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
+    trailingSlash: false,
+    stylesheet: "/rss/styles.xsl",
     items: blogs.map((blog) => ({
       ...blog.data,
       link: `/blog/${blog.id}/`,
+      // Note: this will not process components or JSX expressions in MDX files.
+      content: sanitizeHtml(parser.render(blog.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
     })),
   });
 }
