7ecbb411e93aabb0388783ba951c7496016ae1b0
ChaiyoKung <kananek45@gmail.com>
2025-09-30 00:28:38 +0700

    fix: improve tagToSlug to handle non-alphanumeric chars

--- src/utils/tagToSlug.ts
+++ src/utils/tagToSlug.ts
@@ -9,8 +9,7 @@
 export function tagToSlug(tag: string): string {
   return tag
     .toLowerCase()
-    .replace(/\s+/g, "-")
-    .replace(/[^a-z0-9-]/g, "")
+    .replace(/[^a-z0-9]+/g, "-")
     .replace(/-+/g, "-")
     .replace(/^-+|-+$/g, "");
 }
